0cb202a8c241894eaca9cfc607ac641b711d61cb
ManuelCAZALLA <manuelcm23@hotmail.com>
2024-12-01 19:31:08 +0100

    [MVVM]

---
+++ Data/Service/BaseNetwork.swift
@@ -0,0 +1,8 @@
+//
+//  BaseNetwork.swift
+//  EstéticaÁngela
+//
+//  Created by Manuel Cazalla Colmenero on 1/12/24.
+//
+
+import Foundation
---
+++ Domain/Model.swift
@@ -0,0 +1,10 @@
+//
+//  Model.swift
+//  EstéticaÁngela
+//
+//  Created by Manuel Cazalla Colmenero on 1/12/24.
+//
+
+import Foundation
+
+
